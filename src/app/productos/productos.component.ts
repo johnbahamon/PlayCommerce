18f84a36209c448062b843587c5a69c4e9a0ec76
alecsanderdiaz <john2005_16@yahoo.com>
2018-08-15 22:18:01 -0500

    Creando Vista Productos

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../servicios/api.service';
 
 @Component({
   selector: 'app-productos',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosComponent implements OnInit {
 
-  constructor() { }
+  productos: any[] = [];
+  cargando: boolean = true;
+
+  desde: number = 0;
+  totalProductos: number = 0;
+
+  constructor(
+    private apiService: ApiService
+  ) { }
 
   ngOnInit() {
+    this.cargarProductos();
+  }
+
+  cargarProductos() {
+    this.apiService.peticionGet('productos')
+      .subscribe((data: any) => {
+        this.productos = data.productos;
+        this.cargando = false;
+      });
+  }
+
+  cambiarDesde( valor: number ) {
+
+    const desde = this.desde + valor;
+    console.log(desde);
+
+    if ( desde >= this.totalProductos ) {
+      return;
+    }
+
+    if ( desde < 0 ) {
+      return;
+    }
+
+    this.desde += valor;
+    this.cargarProductos();
+
   }
 
 }
