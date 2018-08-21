7a060efaf5e508b5883d9e2ce0e07e78e8520716
alecsanderdiaz <john2005_16@yahoo.com>
2018-08-20 23:36:51 -0500

    Agregar Descripcion al producto

--- src/app/productos/editar-producto/agregar-descripcion/agregar-descripcion.component.ts
+++ src/app/productos/editar-producto/agregar-descripcion/agregar-descripcion.component.ts
@@ -1,3 +1,5 @@
+import { ApiService } from './../../../servicios/api.service';
+import { UrlFirebaseService } from './../../../servicios/url-firebase.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -8,14 +10,48 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AgregarDescripcionComponent implements OnInit {
 
+  texto: string;
+  alineacion: string;
+
+  coleccion: any[] = [];
+
   productoId: string;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    public servicioURL: UrlFirebaseService,
+    private apiService: ApiService
   ) { }
 
   ngOnInit() {
     this.productoId = this.route.snapshot.params.id;
+  }
+
+  agregarDescripcion() {
+
+    const descripcionArray = [];
+
+    const descripcion = {
+      alineacion: this.alineacion,
+      texto: this.texto,
+      imagen: this.servicioURL.obtenerUrls()[0]
+    };
+
+    descripcionArray.push(descripcion);
+
+    console.log(descripcion);
+
+    this.apiService.peticionesPut('productos/agregar-descripcion/' + this.productoId, descripcionArray)
+      .subscribe((data: any) => {
+        swal('Muy Bien', 'Descripción agregada con éxito', 'success');
+        this.servicioURL.resetear();
+        this.coleccion = [];
+      });
+  }
+
+  loadImages(event) {
+    this.coleccion = event.target.files;
+    console.log(this.coleccion);
   }
 
 }
