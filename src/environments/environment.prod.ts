7c9b39f776f58d97efcd66074beecda467079393
alecsanderdiaz <john2005_16@yahoo.com>
2018-08-23 21:02:23 -0500

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAPYXKvq_EnDzjGr1PA6Jhgb_wcZazyUUg',
+    authDomain: 'universal-de-licuadoras.firebaseapp.com',
+    databaseURL: 'https://universal-de-licuadoras.firebaseio.com',
+    projectId: 'universal-de-licuadoras',
+    storageBucket: 'universal-de-licuadoras.appspot.com',
+    messagingSenderId: '305444689724'
+  },
+  urlApi: 'http://localhost:3001/'
 };
