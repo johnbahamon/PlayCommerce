01f81d92e52cec51a3854f8a68eceaf22ff73c78
John Bahamon <johnbahamon@playcommerce.org>
2019-06-20 21:42:13 -0500

    heroku

--- config/config.js
+++ config/config.js
@@ -1,17 +1,11 @@
-// Variable Puerto
-process.env.PORT = process.env.PORT || 3001;
-// Si no existe NODE_ENV, me aseguro de convertirla en 'dev'
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Lógica para trabajar con dos Bases de datos según el entorno
 let urlDB;
 
-if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'development') {
     console.log('Estamos en modo desarrollo');
 
-    urlDB = 'mongodb+srv://alexander:SBaRkgYxsHfVTvPD@tienda-universal-lx5cm.mongodb.net/admintienda?retryWrites=true';
+    urlDB = process.env.MONGO_URI_LOCAL;
     
-    // urlDB = 'mongodb://192.168.1.100:27017/tienda-universal';
-    // urlDB = 'mongodb://admini:abc123456789@ds121945.mlab.com:21945/tiendaonline';
 } else {
     urlDB = process.env.MONGO_URI;
 }
